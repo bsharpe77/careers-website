0543c0d5cf34993208fd52248fecd5c12163ca05
bsharpe77 <38922383+bsharpe77@users.noreply.github.com>
2023-06-28 21:39:46 +0000

    fixed DB and turned credentials into secret

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 from flask import Flask, render_template, jsonify
-from database import engine
-from sqlalchemy import text
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-
-
-def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    jobs = []
-    for row in result.all():
-      jobs.append(row)
-    return jobs
 
 
 @app.route("/")
@@ -22,7 +12,7 @@ def hello_jovian():
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  return jsonify(load_jobs_from_db())
 
 
 if __name__ == "__main__":
