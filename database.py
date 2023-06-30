38e0e84c520074752b41216489d9b727dee318ad
bsharpe77 <38922383+bsharpe77@users.noreply.github.com>
2023-06-30 19:18:48 +0000

    store application in db - write to DB

--- database.py
+++ database.py
@@ -16,3 +16,28 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(row)
     return jobs
+
+
+#fixed from tutorial code -- had to change the way the variable was handled (id) and change the return statement
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = " + id))
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]
+
+
+#had to refactor how this collected and submitted the variables from the form
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    values = job_id + ",'" + data['full_name'] + "','" + data[
+      'email'] + "','" + data['linkedIn'] + "','" + data[
+        'education'] + "','" + data['work_experience'] + "','" + data[
+          'resume_url'] + "'"
+    query = text(
+      "INSERT INTO applications (job_id,full_name,email,linkedin_url,education,work_experience,resume_url) VALUES ("
+      + values + ")")
+
+    conn.execute(query)
--- notes.md
+++ notes.md
@@ -4,6 +4,8 @@ Free wireframe tool: https://excalidraw.com/
 Bootstrap: https://getbootstrap.com/
 Video: https://youtu.be/yBDHkveJUf4?t=4112
   For Wednesday: Chapter 1.6
+  For Thursday: Chapter 2.5
+  For Friday: Chapter 2.6/2.7 4:04:52
 Mailto link builder: https://mailtolink.me/
 Cloud Hosting: render.com - https://render.com/docs/deploy-flask
 
